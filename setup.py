939a8a169ed98e5f54669b4ee616d739dd02414f
guldfisk <ce.guldfisk@gmail.com>
2018-03-14 20:41:40 +0100

    ok

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
 from setuptools import setup
+import os
+
+def package_files(directory):
+	paths = []
+	for path, directories, file_names in os.walk(directory):
+		for filename in file_names:
+			paths.append(os.path.join('..', path, filename))
+	return paths
+
+extra_files = package_files('loonserver')
 
 setup(
 	name='loonserver',
 	version='1.0',
 	packages=['loonserver'],
+	package_data={'': extra_files},
 	install_requires=[
 		'frozendict',
+		'pysimplesoap',
 	]
 )
